de72e45d7b3852c44b1ae56611a5f128cb59a637
sheetalc818 <sheetalc818@gmail.com>
2020-02-25 15:39:27 +0530

    Added : To Calculate Employee Daily Wage

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,10 +1,19 @@
 public class EmployeeWageBuilder{
 	public static void main(String args[]){
 		int IS_EMP_PRESENT = 1;
+		int EMP_RATE_PER_HR = 20;
+
+		int empHrs = 0;
+		int empWage = 0;
+
 		double empCheck = Math.floor(Math.random() * 10) %2;
-		if ( empCheck == IS_EMP_PRESENT )
+		if ( empCheck == IS_EMP_PRESENT ){
 			System.out.println("Employee is Present");
-		else
+			empHrs=8;
+		}else{
 			System.out.println("Employee is absent");
+		}
+		empWage = empHrs * EMP_RATE_PER_HR;
+		System.out.println("Employee wage :"+empWage);
 	}
 }
