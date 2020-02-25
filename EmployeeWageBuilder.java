acf3b0c1aab3ff00c665dd8f960d57f1128f407c
sheetalc818 <sheetalc818@gmail.com>
2020-02-25 15:49:51 +0530

    Added : Solving using Switch case

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,26 +1,32 @@
 public class EmployeeWageBuilder
 {
+	//Constants
+	public static final int IS_FULL_TIME = 0;
+   public static final int IS_PART_TIME = 1;
+   public static final int EMP_RATE_PER_HR = 20;
+
 	public static void main(String args[])
 	{
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
-		int EMP_RATE_PER_HR = 20;
-
+		//Variables
 		int empHrs = 0;
 		int empWage = 0;
 
-		double empCheck = Math.floor(Math.random() * 10) %2;
+		//Computation
+		int empCheck =(int) Math.floor((Math.random() * 10) %2);
 
-		if ( empCheck == IS_FULL_TIME )
+		switch(empCheck)
 		{
-			System.out.println("Employee is full time");
-			empHrs=8;
+			case IS_FULL_TIME :
+					empHrs = 8;
+					break;
+			case IS_PART_TIME :
+					empHrs = 4;
+					break;
+			default :
+					empHrs = 0;
 		}
-		else if ( empCheck == IS_PART_TIME )
-		{
-			System.out.println("Employee is part time");
-			empHrs=4;
-		}
+
+		//Calculating Emp wage
 		empWage = empHrs * EMP_RATE_PER_HR;
 		System.out.println("Employee wage :"+empWage);
 	}
