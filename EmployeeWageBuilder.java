914eeceb99f8741f3cb004a0235748268ba14a9c
sheetalc818 <sheetalc818@gmail.com>
2020-02-25 15:44:27 +0530

    Added : To Add Part time Emp and Calculate Wage

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,17 +1,25 @@
-public class EmployeeWageBuilder{
-	public static void main(String args[]){
-		int IS_EMP_PRESENT = 1;
+public class EmployeeWageBuilder
+{
+	public static void main(String args[])
+	{
+		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
 		int EMP_RATE_PER_HR = 20;
 
 		int empHrs = 0;
 		int empWage = 0;
 
 		double empCheck = Math.floor(Math.random() * 10) %2;
-		if ( empCheck == IS_EMP_PRESENT ){
-			System.out.println("Employee is Present");
+
+		if ( empCheck == IS_FULL_TIME )
+		{
+			System.out.println("Employee is full time");
 			empHrs=8;
-		}else{
-			System.out.println("Employee is absent");
+		}
+		else if ( empCheck == IS_PART_TIME )
+		{
+			System.out.println("Employee is part time");
+			empHrs=4;
 		}
 		empWage = empHrs * EMP_RATE_PER_HR;
 		System.out.println("Employee wage :"+empWage);
