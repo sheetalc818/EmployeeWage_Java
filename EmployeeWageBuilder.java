03d96371763dd348b36d43dd0368aff37262920a
sheetalc818 <sheetalc818@gmail.com>
2020-02-25 16:19:15 +0530

    Refactor code To write Class Method

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -3,12 +3,24 @@ public class EmployeeWageBuilder
 	//Constants
 	public static final int IS_FULL_TIME = 0;
    public static final int IS_PART_TIME = 1;
-   public static final int EMP_RATE_PER_HR = 20;
-	public static final int NUMBER_OF_DAYS=20;
-	public static final int MAX_HRS_IN_MONTH=100;
 
-	public static void main(String args[])
+	private final String company;
+	private final int empRatePerHr;
+	private final int numOfWorkingDays;
+	private final int maxHrsPerMonth;
+
+	public EmployeeWageBuilder(String company, int empRatePerHr, int numOfWorkingDays, int maxHrsPerMonth)
 	{
+		this.company=company;
+		this.empRatePerHr=empRatePerHr;
+		this.numOfWorkingDays=numOfWorkingDays;
+		this.maxHrsPerMonth=maxHrsPerMonth;
+	}
+
+	public static void main (String args[])
+	{
+		EmployeeWageBuilder dMart = new EmployeeWageBuilder("DMart", 20, 2, 10);
+
 		//Variables
 		int empHrs = 0;
 		int empWage = 0;
@@ -16,11 +28,12 @@ public class EmployeeWageBuilder
 		int totalEmpHrs = 0;
 		int totalWorkingDays = 0;
 		int dailyWage=0;
+
 		//Computation
-		while ( totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUMBER_OF_DAYS )
+		while ( totalEmpHrs <= dMart.maxHrsPerMonth && totalWorkingDays < dMart.numOfWorkingDays )
 		{
 			totalWorkingDays++;
-			int empCheck =(int) Math.floor((Math.random() * 10) %2);
+			int empCheck = (int) Math.floor((Math.random() * 10) %2);
 			switch(empCheck)
 			{
 				case IS_FULL_TIME :
@@ -32,11 +45,12 @@ public class EmployeeWageBuilder
 				default :
 						empHrs = 0;
 			}
+
 			//Calculating Emp wage
-			dailyWage = empHrs * EMP_RATE_PER_HR;
+			dailyWage = empHrs * dMart.empRatePerHr;
 			totalEmpWage += dailyWage;
-			System.out.println("Employee wage :"+dailyWage);
+			System.out.println("Company :"+dMart.company+" Work_Days :"+totalWorkingDays+" Daily_Wage :"+dailyWage);
 		}
-		System.out.println("Total Emp wage:"+totalEmpWage);
+		System.out.println(dMart.company+" Total_Emp_Wage :"+totalEmpWage);
 	}
 }
