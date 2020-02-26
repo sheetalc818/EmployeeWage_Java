0b572cee0814c92f94bbeff6508d89a4e28d3ee9
sheetalc818 <sheetalc818@gmail.com>
2020-02-26 11:09:36 +0530

    Refactor : fixed indentation

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -2,14 +2,14 @@ import java.util.*;
 
 class CompanyEmpWage
 {
-	String company;
+   String company;
    int empRatePerHr;
    int numOfWorkingDays;
    int maxHrsPerMonth;
 
    public CompanyEmpWage(String company, int empRatePerHr, int numOfWorkingDays, int maxHrsPerMonth)
    {
-   	this.company=company;
+      this.company=company;
       this.empRatePerHr=empRatePerHr;
       this.numOfWorkingDays=numOfWorkingDays;
       this.maxHrsPerMonth=maxHrsPerMonth;
@@ -18,7 +18,7 @@ class CompanyEmpWage
 
 class EmployeeWageBuilder
 {
-	//Constants
+   //Constants
    public static final int IS_FULL_TIME = 0;
    public static final int IS_PART_TIME = 1;
 
@@ -46,11 +46,11 @@ class EmployeeWageBuilder
       for (i = 0; i < companyWage.size(); i++) {
       	//Computation
          while (totalEmpHrs <= companyWage.get(i).maxHrsPerMonth && totalWorkingDays < companyWage.get(i).numOfWorkingDays) {
-         	totalWorkingDays++;
+            totalWorkingDays++;
             int empCheck = (int) Math.floor((Math.random() * 10) % 2);
 
-				switch (empCheck) {
-            	case IS_FULL_TIME:
+	 switch (empCheck) {
+               case IS_FULL_TIME:
                         empHrs = 8;
                         break;
                case IS_PART_TIME:
@@ -60,7 +60,7 @@ class EmployeeWageBuilder
                         empHrs = 0;
             }
 
-				//Calculating Emp wage
+	    //Calculating Emp wage
             dailyWage = empHrs * companyWage.get(i).empRatePerHr;
             totalEmpWage += dailyWage;
             System.out.println("Company :" + companyWage.get(i).company + " Work_Days :" + totalWorkingDays + " Daily_Wage :" + dailyWage);
@@ -68,33 +68,32 @@ class EmployeeWageBuilder
        System.out.println(companyWage.get(i).company + " Total_Emp_Wage :" + totalEmpWage);
        System.out.println("");
        hm.put(companyWage.get(i).company, totalEmpWage);
-		}
-		System.out.println("COMPANY TOTAL_WAGE");
-   	for (Map.Entry m : hm.entrySet()) {
-    		System.out.println(m.getKey() + " " + m.getValue());
-   	}
+     }
+     System.out.println("COMPANY TOTAL_WAGE");
+     for (Map.Entry m : hm.entrySet()) {
+    	System.out.println(m.getKey() + " " + m.getValue());
+     }
 
-	   String companyName="Dmart";
-		getTotalWageByCompany(companyName,hm);
- 	}
-
-	public void getTotalWageByCompany(String companyName, Map<String,Integer> hm)
-	{
-   	for (Map.Entry m : hm.entrySet()) {
-      	if (m.getKey() == companyName){
-				System.out.println("");	
-				System.out.println("Total_Wage :"+m.getValue());
-         }
-      }
+     String companyName="Dmart";
+     getTotalWageByCompany(companyName,hm);
+ }
+ public void getTotalWageByCompany(String companyName, Map<String,Integer> hm)
+ {
+   for (Map.Entry m : hm.entrySet()) {
+   if (m.getKey() == companyName){
+	System.out.println("");	
+        System.out.println("Total_Wage :"+m.getValue());
    }
+  }
+ }
 
-	public static void main(String args[])
-   {
-   	EmployeeWageBuilder empWage = new EmployeeWageBuilder();
-      empWage.addCompany("Dmart",20,2,10);
-      empWage.addCompany("Reliance",20,4,10);
+ public static void main(String args[])
+ {
+   EmployeeWageBuilder empWage = new EmployeeWageBuilder();
+   empWage.addCompany("Dmart",20,2,10);
+   empWage.addCompany("Reliance",20,4,10);
 
-      empWage.calculateEmployeeWageForCompany();
-   }
+   empWage.calculateEmployeeWageForCompany();
+ }
 }
 
